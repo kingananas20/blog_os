0e2b46178eb22349d5d4f18becc4a9f15788b191
kingananas20 <jonasarter@outlook.com>
2025-03-23 13:58:06 +0100

    first commit, stuck on cargo bootimage

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// this function is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello World";
+
+#[no_mangle]    // don't mangle the function name
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function named _start
+    let vga_buffer: *mut u8 = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
