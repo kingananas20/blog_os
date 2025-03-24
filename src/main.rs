f22266b7480f4520dec136e7ffdafa94ffbb4b3a
kingananas20 <jonasarter@outlook.com>
2025-03-24 20:28:02 +0100

    worked to the post 04 to the start of 'Testing the VGA Buffer'

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,91 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+mod serial;
 mod vga_buffer;
 
 // this function is the entry point, since the linker looks for a function named _start
 #[no_mangle] // don't mangle the function name
 pub extern "C" fn _start() -> ! {
-    println!("Hello World");
-    println!("This should be on the second line");
+    println!("Hello World!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
 // this function is called on panic
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+//--------------------------------[testing]--------------------------------//
+
+// panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+// test test case
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
+
+// test runner to run all tests like in an std env
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+//--------------------------------[exiting qemu]--------------------------------//
+
+// struct with the qemu exit codes
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+// function to exit qemu with the given exit code
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
