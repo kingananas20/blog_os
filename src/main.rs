1f696ead279925ea5f67e9a63637fb077ca75b63
kingananas20 <jonasarter@outlook.com>
2025-03-24 20:58:43 +0100

    completed the testing post

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod serial;
-mod vga_buffer;
+use blog_os::println;
 
 // this function is the entry point, since the linker looks for a function named _start
 #[no_mangle] // don't mangle the function name
@@ -32,60 +31,5 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-// test test case
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
-}
-
-// test runner to run all tests like in an std env
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-//--------------------------------[exiting qemu]--------------------------------//
-
-// struct with the qemu exit codes
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-// function to exit qemu with the given exit code
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
+    blog_os::test_panic_handler(info)
 }
