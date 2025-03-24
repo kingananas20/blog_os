ac26d34c64828b17e13c3fff8ae59c98d5524ac8
kingananas20 <jonasarter@outlook.com>
2025-03-24 18:56:11 +0100

    removed the panic from the main.rs function

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,11 @@
 
 mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World";
-
 // this function is the entry point, since the linker looks for a function named _start
 #[no_mangle] // don't mangle the function name
 pub extern "C" fn _start() -> ! {
-    let vga_buffer: *mut u8 = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World");
+    println!("This should be on the second line");
 
     loop {}
 }
